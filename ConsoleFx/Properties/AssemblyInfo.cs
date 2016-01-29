a36070ad57fe6b89e7f786557ba7a3612da49e92
Jeevan James <jeevanjj@yahoo.com>
2016-01-30 01:13:46 +0530

    Updated version.

--- ConsoleFx/Properties/AssemblyInfo.cs
+++ ConsoleFx/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ Visit the project URL for documentation or download the full package to obtain t
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("2.0.0-beta7")]
+[assembly: AssemblyInformationalVersion("2.0.0-beta8")]
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
