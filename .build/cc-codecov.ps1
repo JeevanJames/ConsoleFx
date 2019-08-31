b20f779d593ae14de30f9b0ebffc377a505e7216
Jeevan James <jeevanjj@live.com>
2019-08-31 16:49:22 +0530

    Clean up scripts

---
+++ .build/cc-codecov.ps1
@@ -0,0 +1,2 @@
+codecov -f "./test/CmdLine.Parser.Tests/coverage.opencover.xml"
+codecov -f "./test/ConsoleExtensions.Tests/coverage.opencover.xml"
---
+++ .build/cc-reportgenerator.ps1
@@ -0,0 +1,3 @@
+reportgenerator -reports:.\test\CmdLine.Abstractions.Tests\coverage.opencover.xml -targetdir:.\.coverage\CmdLine.Abstractions
+reportgenerator -reports:.\test\CmdLine.Parser.Tests\coverage.opencover.xml -targetdir:.\.coverage\CmdLine.Parser
+reportgenerator -reports:.\test\ConsoleExtensions.Tests\coverage.opencover.xml -targetdir:.\.coverage\ConsoleExtensions
